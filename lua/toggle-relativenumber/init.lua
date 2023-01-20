20274e3737db8353c503e914db21a51d34ef8084
Josh Dirkx <joshdirkx@gmail.com>
2023-01-20 13:27:13 -0800

    name

--- lua/toggle-relativenumber/init.lua
+++ lua/toggle-relativenumber/init.lua
@@ -1,11 +1,8 @@
-local function setup(parameters)
-end
-
-local toggleNumbers = vim.api.nvim_create_augroup('toggleNumbers', { clear = true })
+local toggleRelativenumber = vim.api.nvim_create_augroup('toggleRelativenumber', { clear = true })
 
 vim.api.nvim_create_autocmd({ 'BufEnter', 'FocusGained', 'InsertLeave', 'WinEnter' }, {
   desc = 'Enable relative line numbers when entering a buffer',
-  group = toggleNumbers,
+  group = toggleRelativenumber,
   callback = function ()
     vim.wo.relativenumber = true
   end
@@ -13,12 +10,9 @@ vim.api.nvim_create_autocmd({ 'BufEnter', 'FocusGained', 'InsertLeave', 'WinEnte
 
 vim.api.nvim_create_autocmd({ 'BufLeave', 'FocusLost', 'InsertEnter', 'WinLeave' }, {
   desc = 'Remove relative line numbers when leaving a buffer',
-  group = toggleNumbers,
+  group = toggleRelativenumber,
   callback = function ()
     vim.wo.relativenumber = false
   end
 })
 
-return {
-  setup = setup,
-}
