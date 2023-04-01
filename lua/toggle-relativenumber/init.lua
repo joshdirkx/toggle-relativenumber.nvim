015273f4cfae0dc6b7fe61e75896108354485fc2
Josh Dirkx <joshdirkx@gmail.com>
2023-04-01 12:03:59 -0700

    stuff

--- lua/toggle-relativenumber/init.lua
+++ lua/toggle-relativenumber/init.lua
@@ -1,8 +1,6 @@
 local function enable_number_relativenumber_current_buffer()
-  vim.api.nvim_win_set_option(0, 'number', true)
-  vim.api.nvim_win_set_option(0, 'relativenumber', true)
-
   vim.api.nvim_command([[autocmd BufLeave,WinLeave,TabLeave * setlocal norelativenumber]])
+  vim.api.nvim_command([[autocmd BufEnter,WinEnter,TabEnter * setlocal number relativenumber]])
 end
 
 enable_number_relativenumber_current_buffer()
